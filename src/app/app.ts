995b18d2f11e3cd4b2761c0a6dc0bbb817a4561b
Natalia-Timote <nataliamirian.nt@gmail.com>
2026-02-01 11:29:00 -0300

    Adicionando logo e criando o cabeçalho

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Container } from './components/container/container';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Container],
+  imports: [RouterOutlet, Container, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
